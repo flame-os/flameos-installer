12a82999bcb58c7231aa24ca5f95060087ae7fa9
aislxflames <proarghaop@gmail.com>
2025-09-11 23:26:25 +0530

    Fix Flamedots install.sh to use USERNAME variable and run commands properly in chroot

--- dotfiles/Flamedots/install.sh
+++ dotfiles/Flamedots/install.sh
@@ -1,76 +1,62 @@
 #!/bin/bash
 set -e
 
-echo "Starting FlameOS dotfiles auto-installation..."
+echo "Installing Flamedots for user: $USERNAME"
 
-# Update system and install dependencies
-sudo pacman -Syu lolcat fzf --noconfirm
+USER_HOME="/home/$USERNAME"
 
-USER_HOME="/home/$(logname)"
-
-# Install yay
-sudo chown -R $(logname):$(logname) /home/$(logname)
-sudo chown -R $(logname):$(logname) /opt
-sudo rm -rf /opt/yay
+# Install yay if not present
 if ! command -v yay &> /dev/null; then
-    git clone https://aur.archlinux.org/yay.git /opt/yay
-    sudo chown -R $(logname):$(logname) /opt/yay
-    cd /opt/yay && makepkg -si --noconfirm
+    echo "Installing yay..."
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    sudo -u $USERNAME makepkg -si --noconfirm
+    rm -rf /tmp/yay
 fi
 
-# Install packages
+# Install packages from packages.sh if it exists
 if [ -f "./packages.sh" ]; then
+    echo "Installing packages..."
     mapfile -t packages < <(grep -vE '^\s*#|^\s*$' ./packages.sh | tr -d '\r')
     for pkg in "${packages[@]}"; do
         if ! pacman -Qi "$pkg" &>/dev/null; then
-            yay -S --noconfirm "$pkg"
+            sudo -u $USERNAME yay -S --noconfirm "$pkg"
         fi
     done
 fi
 
-# Copy dotfiles
-if [ -d "./dotfiles/" ]; then
-    rsync -avi "./dotfiles/" "$USER_HOME/"
+# Install GRUB theme if available
+if [ -d "./rootfiles/grub" ]; then
+    echo "Installing GRUB theme..."
+    mkdir -p /boot/grub/themes/
+    cp -rf ./rootfiles/grub/* /boot/grub/themes/
+    sed -i '/^GRUB_THEME=/d' /etc/default/grub
+    echo 'GRUB_THEME="/boot/grub/themes/Castorice/theme.txt"' >> /etc/default/grub
+    grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
-# Install grub theme
-if [ -d "rootfiles/grub/Castorice" ]; then
-    sudo mkdir -p /boot/grub/themes/
-    sudo cp -rf rootfiles/grub/Castorice /boot/grub/themes/Castorice
-    sudo sed -i '/^GRUB_THEME=/d' /etc/default/grub
-    echo 'GRUB_THEME="/boot/grub/themes/Castorice/theme.txt"' | sudo tee -a /etc/default/grub
-    sudo grub-mkconfig -o /boot/grub/grub.cfg
+# Install SDDM theme if available
+if [ -d "./rootfiles/sddm" ]; then
+    echo "Installing SDDM theme..."
+    cp -rf ./rootfiles/sddm/* /usr/share/sddm/themes/
+    echo -e '[Theme]\nCurrent=Candy' > /etc/sddm.conf
 fi
 
-# Install sddm theme
-if [ -d "rootfiles/sddm/Candy" ]; then
-    sudo cp -rf rootfiles/sddm/Candy /usr/share/sddm/themes/Candy
-    sudo mkdir -p /etc
-    echo -e '[Theme]\nCurrent=Candy' | sudo tee /etc/sddm.conf
+# Setup zsh with oh-my-zsh and powerlevel10k
+echo "Setting up zsh..."
+sudo -u $USERNAME bash -c '
+    rm -rf ~/.oh-my-zsh
+    CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+'
+chsh -s $(which zsh) $USERNAME
+
+# Copy environment files if available
+if [ -f "./rootfiles/environment" ]; then
+    cp ./rootfiles/environment /etc/environment
 fi
 
-# Setup zsh
-sudo rm -rf ~/.oh-my-zsh
-CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-chsh -s $(which zsh)
+# Set proper ownership of all user files
+chown -R $USERNAME:$USERNAME $USER_HOME
 
-if [ -f "./dotfiles/.zshrc" ]; then
-    cp -rf ./dotfiles/.zshrc $USER_HOME/.zshrc
-fi
-if [ -f "./dotfiles/.p10k.zsh" ]; then
-    cp -rf ./dotfiles/.p10k.zsh $USER_HOME/.p10k.zsh
-fi
-
-# Copy environment files
-if [ -f "rootfiles/environment" ]; then
-    sudo cp -r rootfiles/environment /etc/environment
-fi
-
-# Enable services
-sudo systemctl enable sddm
-sudo systemctl enable NetworkManager
-sudo systemctl enable bluetooth
-
-echo "FlameOS dotfiles installation completed!"
-echo "Reboot to apply all changes."
+echo "Flamedots installation completed for $USERNAME!"
